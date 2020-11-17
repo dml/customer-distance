c25231258f0202d93873d165f009481e0530e212
Dmitry Larkin <dmitry.larkin@gmail.com>
2020-11-17 22:28:57 +0100

    absolute longitude difference

--- lib/distance_calculator.rb
+++ lib/distance_calculator.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class DistanceCalculator
+  attr_reader :source_longitude,
+              :source_latitude,
+              :destination_longitude,
+              :destination_latitude
+
+  def initialize(longitude, latitude)
+    @source_longitude = longitude
+    @source_latitude = latitude
+  end
+
+  def absolute_longitude_difference
+    (destination_longitude - source_longitude).abs
+  end
+
   def self.degrees_to_radians(degrees)
     degrees.to_f * Math::PI / 180
   end
--- spec/lib/distance_calculator_spec.rb
+++ spec/lib/distance_calculator_spec.rb
@@ -2,27 +2,48 @@
 
 require_relative '../../lib/distance_calculator'
 
-RSpec.describe DistanceCalculator do
-  subject { calculator }
+RSpec.describe DistanceCalculator, '.degrees_to_radians' do
+  subject { calculator.degrees_to_radians(degtrees) }
 
   let(:calculator) { described_class }
 
-  describe '.degrees_to_radians' do
-    subject { calculator.degrees_to_radians(degtrees) }
+  [
+    { degtrees: 0, radians: 0 },
+    { degtrees: 90, radians: Math::PI / 2 },
+    { degtrees: 180, radians: Math::PI },
+    { degtrees: -90, radians: -1 * Math::PI / 2 },
+    { degtrees: -180, radians: -1 * Math::PI }
+  ].each do |fixture|
+    context "when #{fixture[:degtrees]} degrees passed" do
+      let(:degtrees) { fixture[:degtrees] }
+      let(:radians) { fixture[:radians] }
 
-    [
-      { degtrees: 0, radians: 0 },
-      { degtrees: 90, radians: Math::PI / 2 },
-      { degtrees: 180, radians: Math::PI },
-      { degtrees: -90, radians: -1 * Math::PI / 2 },
-      { degtrees: -180, radians: -1 * Math::PI }
-    ].each do |fixture|
-      context "when #{fixture[:degtrees]} degrees passed" do
-        let(:degtrees) { fixture[:degtrees] }
-        let(:radians) { fixture[:radians] }
+      it { is_expected.to eq(radians) }
+    end
+  end
+end
 
-        it { is_expected.to eq(radians) }
+RSpec.describe DistanceCalculator, '#absolute_longitude_difference' do
+  subject { calculator.absolute_longitude_difference }
+
+  let(:calculator) { described_class.new(0, 0) }
+
+  [
+    { source: 0, destination: 0, expected: 0 },
+    { source: -10, destination: 10, expected: 20 },
+    { source: 10, destination: -10, expected: 20 },
+    { source: -10, destination: -20, expected: 10 },
+    { source: 10, destination: 20, expected: 10 }
+  ].each do |fixture|
+    context "when source #{fixture[:source]} and destination #{fixture[:destination]}" do
+      let(:expected) { fixture[:expected] }
+
+      before do
+        allow(calculator).to receive(:source_longitude).and_return(fixture[:source])
+        allow(calculator).to receive(:destination_longitude).and_return(fixture[:destination])
       end
+
+      it { is_expected.to eq(expected) }
     end
   end
 end
